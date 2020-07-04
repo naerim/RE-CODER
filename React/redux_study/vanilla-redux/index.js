0563f45b80fe0321f094df7374d9a52a72154687
naerim <naerim1119@gmail.com>
2020-07-05 00:57:23 +0900

    Add reducer function

---
+++ React/redux_study/vanilla-redux/index.js
@@ -0,0 +1,45 @@
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector("#decrease");
+
+// 액션 이름은 문자열 형태로, 주로 대문자
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+// 액션 생성 함수
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = (difference) => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+// 초깃값 정의
+const initialState = {
+  toggle: false,
+  counter: 0,
+};
+
+// 리듀서 함수 정의
+// state가 undefined일 때는 initialState를 기본값으로 사용
+function reducer(state = initialState, action) {
+  //action.type에 따라 다른 작업을 처리함
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state, // 분별성 유지를 해 주어야 한다.
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+}
